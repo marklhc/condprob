5b53c5e195cc53f08697a7f07bb2004df0d392dd
marklhc <mark.lai@uc.edu>
2017-01-26 12:45:29 -0500

    Change settings for the conditional distributions

--- app.R
+++ app.R
@@ -9,6 +9,15 @@
 
 library(shiny)
 library(plotly)
+library(MASS)
+post <- readRDS("schools_post.RDS")
+mu <- post[ , "mu"]; tau <- post[ , "tau"]
+kd <- kde2d(mu, tau, n = 50, h = c(width.SJ(mu), width.SJ(tau)))
+p <- plot_ly(x = kd$x, y = kd$y, z = kd$z) %>% add_surface()
+
+kd2 <- kde2d(mu, tau, n = c(77, 73), 
+             h = c(width.SJ(mu), width.SJ(tau)), 
+             lims = c(round(range(mu), 0), round(range(tau), 0)))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -21,14 +30,14 @@ ui <- fluidPage(
       sidebarPanel(
          sliderInput("x",
                      "x-value",
-                     min = 1,
-                     max = 61,
-                     value = 1), 
+                     min = round(min(mu), 0),
+                     max = round(max(mu), 0),
+                     value = 0), 
          sliderInput("y",
                      "y-value",
-                     min = 1,
-                     max = 87,
-                     value = 1)
+                     min = round(min(tau), 0),
+                     max = round(max(tau), 0),
+                     value = 0)
       ),
       
       # Show a plot of the generated distribution
@@ -42,14 +51,14 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    output$surfPlot <- renderPlotly({
-     plot_ly(z = ~volcano) %>% add_surface()
+     p
    })
    output$condPlot <- renderPlotly({
-     p1 <- plot_ly() %>% add_lines(x = seq_along(volcano[ , input$x]), 
-                                   y = volcano[ , input$x]) %>%
+     p1 <- plot_ly() %>% add_lines(x = kd2$y,
+                                   y = kd2$z[which(kd2$x == input$x), ]) %>%
        layout(xaxis = list(title = paste0("P(Y | X = ", input$x, ")")))
-     p2 <- plot_ly() %>% add_lines(x = seq_along(volcano[input$y , ]), 
-                                   y = volcano[input$y , ]) %>%
+     p2 <- plot_ly() %>% add_lines(x = kd2$x,
+                                   y = kd2$z[ , which(kd2$y == input$y)]) %>%
        layout(xaxis = list(title = paste0("P(X | Y = ", input$y, ")")))
      subplot(p1, p2, titleX = TRUE)
    })
